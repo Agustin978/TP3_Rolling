778b0dca545e0091f3a4df26f04d73dab028266a
Agustin978 <loboagus10@gmail.com>
2023-03-13 08:27:26 -0300

    Agregado un poco de estilo.

--- Desarrollo/js/Ejercicio1_Array.js
+++ Desarrollo/js/Ejercicio1_Array.js
@@ -1,6 +1,6 @@
 let meses = ['Enero','Febrero','Marzo','Abril','Mayo','Junio','Julio','Agosto','Septiembre','Octubre','Noviembre','Diciembre'];
 
-document.write("<section class='container'> <div class='card'> <ul class='list-group list-group-flush'>");
+document.write("<section class='container'> <div class='card mx-auto'> <ul class='list-group list-group-flush'>");
 for(let i = 0; i < meses.length; i++)
 {
     document.write("<li class='list-group-item'>"+meses[i]+"</li>");
